d3ba18346863b7f68d2fcc7213a7d12d366599ca
manhkaka99 <manhkaka99ns@gmail.com>
2023-11-28 23:35:04 +0700

    Update

--- Commands/TagSite.cs
+++ Commands/TagSite.cs
@@ -15,6 +15,7 @@ using Application = Autodesk.Revit.ApplicationServices.Application;
 using Nice3point.Revit.Toolkit.External;
 using System.Windows.Documents;
 using System.Windows.Controls;
+using System.Collections;
 
 #endregion
 
@@ -34,13 +35,26 @@ namespace BimIshou.Commands
 
             FilteredElementCollector tagIds = new FilteredElementCollector(doc, doc.ActiveView.Id).OfCategory(BuiltInCategory.OST_SiteTags).WhereElementIsNotElementType();
 
-            ICollection<Element> siteTag = new List<Element>();
+            ICollection<Element> siteTagType = new List<Element>();
 
             foreach (Element a in tagIds)
             {
                 if (a.get_Parameter(BuiltInParameter.ELEM_FAMILY_PARAM).AsValueString() == "dタグ_外構_計画と現況レベル")
                 {
-                    siteTag.Add(a);
+                    siteTagType.Add(a);
+                }
+            }
+            ICollection<Element> siteTag2 = new List<Element>();
+            ICollection<Element> siteTag3 = new List<Element>();
+            foreach (Element a in siteTagType)
+            {
+                if (a.Name == "計画のみ")
+                {
+                    siteTag2.Add(a);
+                }
+                else if (a.Name == "計画と現況")
+                {
+                    siteTag3.Add(a);
                 }
             }
             FamilySymbol tagId1 = (from tag in new FilteredElementCollector(doc)
@@ -56,7 +70,9 @@ namespace BimIshou.Commands
 
             FilteredElementCollector collector = new FilteredElementCollector(doc)
                 .OfClass(typeof(RevitLinkInstance));
-            
+            IList<Element> list = new List<Element>();
+            IList<Element> list2 = new List<Element>(); //List cao do 0
+            IList<Element> list3 = new List<Element>(); //List cao do khac 0
             foreach (Element element in collector)
             {
                 RevitLinkInstance instance = element as RevitLinkInstance;
@@ -65,59 +81,84 @@ namespace BimIshou.Commands
                 FilteredElementCollector sites = new FilteredElementCollector(linkDoc)
                                    .OfCategory(BuiltInCategory.OST_Site)
                                    .WhereElementIsNotElementType();
-                IList<Element> list = new List<Element>();
                 foreach (Element site in sites)
                 {
-
                     ElementType elementType = instance.GetLinkDocument().GetElement(site.GetTypeId()) as ElementType;
-
                     if (elementType.FamilyName == "d外構_レベル標_隅" || elementType.FamilyName == "d外構_レベル標_中心")
                     {
+                        if (site.LookupParameter("現況レベル").AsDouble() == 0 || site.LookupParameter("現況レベル").AsDouble() == null)
+                        {
+                            list2.Add(element);
+                        }
+                        else
+                        {
+                            list3.Add(element);
+                        }
                         list.Add(site);
                     }
                 }
-                Checktag checkTag = new Checktag();
-                try
-                {
-                    using (Transaction trans = new Transaction(doc, "Creat Tag"))
-                    {
-                        trans.Start();
-                        foreach (Element ele in list)
-                        {
-
-                            if (checkTag.checkTag(doc, ele, siteTag) == false)
-                            {
-
-                                Reference reference = new Reference(ele).CreateLinkReference(instance);
-                                LocationPoint loc = ele.Location as LocationPoint;
-                                XYZ pos = loc.Point;
-                                if (ele.LookupParameter("現況レベル").AsDouble() == 0 || ele.LookupParameter("現況レベル").AsDouble() == null)
-                                {
-                                    IndependentTag tag = IndependentTag.Create(doc, tagId1.Id, doc.ActiveView.Id, reference, true, tOrien, pos);
-                                    LinkElementId linkId = tag.TaggedElementId;
-                                    ElementId linkInsancetId = linkId.LinkInstanceId;
-                                    ElementId linkedElementId = linkId.LinkedElementId;
-                                }
-                                else
-                                {
-                                    IndependentTag tag = IndependentTag.Create(doc, tagId2.Id, doc.ActiveView.Id, reference, true, tOrien, pos);
-                                    LinkElementId linkId = tag.TaggedElementId;
-                                    ElementId linkInsancetId = linkId.LinkInstanceId;
-                                    ElementId linkedElementId = linkId.LinkedElementId;
-                                }
-                            }
-                        }
-                        trans.Commit();
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    message = ex.Message;
-                    return Result.Failed;
-                }
             }
-            
+            foreach(Element element in list2)
+            {
+                TaskDialog.Show("Test", element.Id.ToString());
+            }
+            //Checktag checkTag = new Checktag();
+            //CheckTag5 checkTag5 = new CheckTag5();
+            //foreach (Element element in collector)
+            //{
+            //    RevitLinkInstance instance = element as RevitLinkInstance;
+            //    try
+            //    {
+            //        using (Transaction trans = new Transaction(doc, "Creat Tag"))
+            //        {
+            //            trans.Start();
+            //            foreach (Element ele in list)
+            //            {
+            //                if (checkTag.checkTag(doc, ele, siteTagType) == false)
+            //                {
+            //                    Reference reference = new Reference(ele).CreateLinkReference(instance);
+            //                    LocationPoint loc = ele.Location as LocationPoint;
+            //                    XYZ pos = loc.Point;
+            //                    if (ele.LookupParameter("現況レベル").AsDouble() == 0 || ele.LookupParameter("現況レベル").AsDouble() == null)
+            //                    {
+            //                        IndependentTag tag = IndependentTag.Create(doc, tagId1.Id, doc.ActiveView.Id, reference, true, tOrien, pos);
+            //                        LinkElementId linkId = tag.TaggedElementId;
+            //                        ElementId linkInsancetId = linkId.LinkInstanceId;
+            //                        ElementId linkedElementId = linkId.LinkedElementId;
+            //                    }
+            //                    else
+            //                    {
+            //                        IndependentTag tag = IndependentTag.Create(doc, tagId2.Id, doc.ActiveView.Id, reference, true, tOrien, pos);
+            //                        LinkElementId linkId = tag.TaggedElementId;
+            //                        ElementId linkInsancetId = linkId.LinkInstanceId;
+            //                        ElementId linkedElementId = linkId.LinkedElementId;
+            //                    }
+            //                }
+            //            }
+            //            foreach (Element ele2 in siteTag2)
+            //            {
+            //                if (checkTag5.checkTag(doc, list2, ele2) == false)
+            //                {
+            //                    doc.Delete(ele2.Id);
+            //                }
+            //            }
+            //            //foreach (Element ele3 in siteTag3)
+            //            //{
+            //            //    if (checkTag5.checkTag(doc, list3, ele3) == false)
+            //            //    {
+            //            //        doc.Delete(ele3.Id);
+            //            //    }
+            //            //}
+            //            trans.Commit();
+            //        }
+
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        message = ex.Message;
+            //        return Result.Failed;
+            //    }
+            //}
             return Result.Succeeded;
         }
     }
@@ -131,6 +172,22 @@ namespace BimIshou.Commands
             {
                 IndependentTag tag = tagId as IndependentTag;
                 if (tag != null && tag.GetTaggedReference().LinkedElementId == elementId)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    internal class CheckTag5
+    {
+        public bool checkTag(Document doc, IList<Element> element, Element tags)
+        {
+            IndependentTag tag2 = tags as IndependentTag;
+            ElementId tagId = tag2.GetTaggedReference().LinkedElementId;
+            foreach (Element ele in element)
+            {
+                if (ele.Id != null && ele.Id == tagId)
                 {
                     return true;
                 }
--- Commands/TagSite_Project.cs
+++ Commands/TagSite_Project.cs
@@ -15,6 +15,7 @@ using Application = Autodesk.Revit.ApplicationServices.Application;
 using Nice3point.Revit.Toolkit.External;
 using System.Windows.Documents;
 using System.Windows.Controls;
+using System.Windows.Markup;
 
 #endregion
 
@@ -31,16 +32,29 @@ namespace BimIshou.Commands
 
             TagMode tMode = TagMode.TM_ADDBY_CATEGORY;
             TagOrientation tOrien = TagOrientation.Horizontal;
-            #region Lấy về tag
+            #region Lấy về tag đã tag
             FilteredElementCollector tagIds = new FilteredElementCollector(doc, doc.ActiveView.Id).OfCategory(BuiltInCategory.OST_SiteTags).WhereElementIsNotElementType();
 
-            ICollection<Element> siteTag = new List<Element>();
+            ICollection<Element> siteTagType = new List<Element>();
 
             foreach (Element a in tagIds)
             {
                 if (a.get_Parameter(BuiltInParameter.ELEM_FAMILY_PARAM).AsValueString() == "dタグ_外構_計画と現況レベル")
                 {
-                    siteTag.Add(a);
+                    siteTagType.Add(a);
+                }
+            }
+            ICollection<Element> siteTag2 = new List<Element>();
+            ICollection<Element> siteTag3 = new List<Element>();
+            foreach (Element a in siteTagType)
+            {
+                if (a.Name == "計画のみ")
+                {
+                    siteTag2.Add(a);
+                }
+                else if (a.Name == "計画と現況")
+                {
+                    siteTag3.Add(a);
                 }
             }
             FamilySymbol tagId1 = (from tag in new FilteredElementCollector(doc)
@@ -54,48 +68,65 @@ namespace BimIshou.Commands
                                    where tag.Name == "計画と現況"
                                    select tag).First();
             #endregion
-
             FilteredElementCollector sites = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Site).WhereElementIsNotElementType();
-            Checktag2 checkTag = new Checktag2();
+            IList<Element> list1 = new List<Element>();
+            IList<Element> list2 = new List<Element>(); //List cao do 0
+            IList<Element> list3 = new List<Element>(); //List cao do khac 0
+            foreach (Element element in sites)
+            {
+                String a = element.LookupParameter("Family").AsValueString();
+
+                if (a == "d外構_レベル標_隅" || a == "d外構_レベル標_中心")
+                {
+                    if (element.LookupParameter("現況レベル").AsDouble() == 0 || element.LookupParameter("現況レベル").AsDouble() == null)
+                    {
+                        list2.Add(element);
+                    }
+                    else
+                    {
+                        list3.Add(element);
+                    }
+                    list1.Add(element);
+                }
+            }
+            Checktag3 checkTag3 = new Checktag3();
+            CheckTag4 checkTag4 = new CheckTag4();
             try
             {
                 using (Transaction trans = new Transaction(doc, "Creat Tag"))
                 {
                     trans.Start();
-                    foreach (Element element in sites)
+                    
+                    foreach (Element ele in list1)
                     {
-                        IList<Element> list = new List<Element>();
-
-
-                        String a = element.LookupParameter("Family").AsValueString();
-
-                        if (a == "d外構_レベル標_隅" || a == "d外構_レベル標_中心")
+                        Autodesk.Revit.DB.Reference reference = new Autodesk.Revit.DB.Reference(ele);
+                        LocationPoint loc = ele.Location as LocationPoint;
+                        XYZ pos = loc.Point;
+                        if (checkTag3.checkTag(doc, ele, siteTagType) == false)
                         {
-                            list.Add(element);
-                        }
-
-
-
-                        foreach (Element ele in list)
-                        {
-
-                            if (checkTag.checkTag(doc, ele, siteTag) == false)
+                            if (ele.LookupParameter("現況レベル").AsDouble() == 0 || ele.LookupParameter("現況レベル").AsDouble() == null)
                             {
-
-                                Reference reference = new Reference(ele);
-                                LocationPoint loc = ele.Location as LocationPoint;
-                                XYZ pos = loc.Point;
-                                if (ele.LookupParameter("現況レベル").AsDouble() == 0 || ele.LookupParameter("現況レベル").AsDouble() == null)
-                                {
-                                    IndependentTag tag = IndependentTag.Create(doc, tagId1.Id, doc.ActiveView.Id, reference, true, tOrien, pos);
-                                }
-                                else
-                                {
-                                    IndependentTag tag = IndependentTag.Create(doc, tagId2.Id, doc.ActiveView.Id, reference, true, tOrien, pos);
-                                }
+                                IndependentTag tag = IndependentTag.Create(doc, tagId1.Id, doc.ActiveView.Id, reference, true, tOrien, pos);
+                            }
+                            else
+                            {
+                                IndependentTag tag = IndependentTag.Create(doc, tagId2.Id, doc.ActiveView.Id, reference, true, tOrien, pos);
                             }
                         }
-
+                    }
+                    foreach (Element ele2 in siteTag2)
+                    {
+                        if (checkTag4.checkTag(doc, list2, ele2) == false)
+                        {
+                            doc.Delete(ele2.Id);
+                        }
+                    }
+                    foreach (Element ele3 in siteTag3)
+                    {
+                        if (checkTag4.checkTag(doc, list3, ele3) == false)
+                        {
+                            doc.Delete(ele3.Id);
+                        }
                     }
                     trans.Commit();
                 }
@@ -105,14 +136,10 @@ namespace BimIshou.Commands
                 message = ex.Message;
                 return Result.Failed;
             }
-
-
-
-
             return Result.Succeeded;
         }
     }
-    internal class Checktag2
+    internal class Checktag3
     {
         public bool checkTag(Document doc, Element element, ICollection<Element> tagIds)
         {
@@ -122,6 +149,22 @@ namespace BimIshou.Commands
             {
                 IndependentTag tag = tagId as IndependentTag;
                 if (tag != null && tag.TaggedLocalElementId == elementId)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    internal class CheckTag4
+    {
+        public bool checkTag(Document doc, IList<Element> element, Element tags)
+        {
+            IndependentTag tag2 = tags as IndependentTag;
+            ElementId tagId = tag2.TaggedLocalElementId;
+            foreach (Element ele in element)
+            {
+                if (ele.Id != null && ele.Id == tagId)
                 {
                     return true;
                 }
