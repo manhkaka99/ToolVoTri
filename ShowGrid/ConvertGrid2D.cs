15a81a58ec5503d7f4d046ac470a3a829dee2452
manhkaka99 <manhkaka99ns@gmail.com>
2024-05-05 16:27:26 +0700

    Update

---
+++ ShowGrid/ConvertGrid2D.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BimIshou.ShowGrid
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ConvertGrid2D : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            FilteredElementCollector grids = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                .OfCategory(BuiltInCategory.OST_Grids)
+                .WhereElementIsNotElementType();
+            using (Transaction trans = new Transaction(doc, "Convert Grid"))
+            {
+                trans.Start();
+                foreach (Element element in grids)
+                {
+                    Grid grid = element as Grid;
+                    grid.SetDatumExtentType(DatumEnds.End0, doc.ActiveView, DatumExtentType.Model);
+                    grid.SetDatumExtentType(DatumEnds.End1, doc.ActiveView, DatumExtentType.Model);
+                }
+                trans.Commit();
+            }
+            return Result.Succeeded;
+        }
+    }
+}
